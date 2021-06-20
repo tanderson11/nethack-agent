9e0cc848918d98051d5ae6e503e6690d28636f1a
Christian Anderson <canderson13@gmail.com>
2021-06-20 08:54:21 -0700

    First class handling of message weirdness

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -38,15 +38,59 @@ class BLStats():
     def get(self, key):
         return self.raw[self.__class__.bl_meaning.index(key)]
 
+class Message():
+    known_lost_messages = set([
+        "Things that are here:",
+        "There is a doorway here.",
+        "There is an open door here.",
+        "Things that you feel here:",
+        "Other things that are here:",
+        "There is a staircase up here.",
+        "There is a staircase down here.",
+        ])
+    def __init__(self, message, tty_chars):
+        self.raw_message = message
+        self.message = ''
+        self.has_more = False
+
+        if np.count_nonzero(message) > 0:
+            self.message = bytes(message).decode('ascii').rstrip('\x00')
+
+        ascii_top_line = bytes(tty_chars[0]).decode('ascii')
+        potential_message = ascii_top_line.strip(' ')
+        if not self.message and potential_message:
+            if not (potential_message.startswith("You read: ") or potential_message in self.__class__.known_lost_messages):
+                import pdb; pdb.set_trace()
+                pass
+            self.message = potential_message
+
+        ascii_top_lines = ascii_top_line + bytes(tty_chars[1:3]).decode('ascii')
+        if "--More--" in ascii_top_lines or "hings that" in ascii_top_lines:
+            # NLE doesn't pass through the --More-- in its observation
+            # ascii_message = bytes(observation['message']).decode('ascii')
+            # So let's go look for it
+            # With pile_limit > 1, we can get a --More-- without a message
+            # # np.count_nonzero(observation['message']) > 0
+            self.has_more = True
+
+        truly_has_more = "--More--" in bytes(tty_chars).decode('ascii')
+
+        if truly_has_more != self.has_more:
+            import pdb; pdb.set_trace()
+            self.has_more = truly_has_more
+
+    def __bool__(self):
+        return bool(self.message)
+
 class MenuPlan():
     def __init__(self, match_to_keypress):
         self.match_to_keypress = match_to_keypress
         self.keypress_count = 0
 
-    def interact(self, ascii_message):
+    def interact(self, message_obj):
         # import pdb; pdb.set_trace()
         for k, v in self.match_to_keypress.items():
-            if k in ascii_message:
+            if k in message_obj.message:
                 self.keypress_count += 1
                 return v
         if self.keypress_count == 0:
@@ -120,6 +164,7 @@ class CustomAgent(BatchedAgent):
 
     def step(self, run_state, observation, reward, done, info):
         ARS.set_active(run_state)
+
         # run_state stuff: Currently only for logging
         run_state.update(done, reward, observation)
         if done:
@@ -129,11 +174,13 @@ class CustomAgent(BatchedAgent):
         if run_state.step_count % 1000 == 0:
             print_stats(run_state, BLStats(observation['blstats']))
 
-        if np.count_nonzero(observation['message']) > 0:
-            message = bytes(observation['message']).decode('ascii')
-            run_state.log_message(message)
-        else:
-            message = ''
+        message = Message(observation['message'], observation['tty_chars'])
+        run_state.log_message(message.message)
+
+        if message.has_more:
+            retval = nethack.ACTIONS.index(nethack.actions.TextCharacters.SPACE)
+            run_state.log_action(retval)
+            return retval
 
         if message:
             retval = run_state.run_menu_plan(message)
@@ -142,25 +189,6 @@ class CustomAgent(BatchedAgent):
                 return retval
 
         possible_actions = list(nethack.actions.CompassDirection)
-
-        # Look at the message to figure out whether we need to press space..
-
-        ascii_top_lines = bytes(observation['tty_chars'][0:3]).decode('ascii')
-        # Can see here or feel here. Things can be lowercase (I forget why)
-        if "--More--" in ascii_top_lines or "hings that" in ascii_top_lines:
-            # NLE doesn't pass through the --More-- in its observation
-            # ascii_message = bytes(observation['message']).decode('ascii')
-            # So let's go look for it
-            # With pile_limit > 1, we can get a --More-- without a message
-            # # np.count_nonzero(observation['message']) > 0
-            retval = nethack.ACTIONS.index(nethack.actions.TextCharacters.SPACE)
-            run_state.log_action(retval)
-            return retval
-        elif "--More--" in bytes(observation['tty_chars']).decode('ascii'):
-            # import pdb; pdb.set_trace()
-            retval = nethack.ACTIONS.index(nethack.actions.TextCharacters.SPACE)
-            run_state.log_action(retval)
-            return retval
 
         if BLStats(observation['blstats']).get('hunger_state') > 2:
             try:
