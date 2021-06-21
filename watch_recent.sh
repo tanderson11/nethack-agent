97bc7513f6a0e0d020b97ddbe96047951472b444
Thayer Anderson <thayer@Thayers-MacBook-Pro.local>
2021-06-21 12:59:16 -0400

    fixed off by one

--- watch_recent.sh
+++ watch_recent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 latest_dir=$(ls -t nle_data/ | head -n1)
-nth_file=$(ls -t nle_data/${latest_dir} | tail -n$1 | head -n1)
+nth_file=$(ls -t nle_data/${latest_dir} | tail -n$(($1 + 1)) | head -n1)
 echo $nth_file
 speed=0.01
 if [ $# -eq 2 ];
