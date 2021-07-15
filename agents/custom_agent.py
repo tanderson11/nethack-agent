9fdca3dc58001dc48cf0e639fb1fdef087708370
Thayer Anderson <thayer@Thayers-MacBook-Pro.local>
2021-07-15 18:21:07 -0400

    Partial refactor with physics and threatmap object, and more utilities

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -14,6 +14,8 @@ from agents.base import BatchedAgent
 import advisors as advs
 import menuplan
 import utilities
+import physics
+
 from utilities import ARS
 import glyphs as gd
 import environment
@@ -48,7 +50,7 @@ class RecordedMonsterDeath():
         try:
             self.monster_glyph = gd.get_by_name(gd.MonsterAlikeGlyph, self.monster_name)
         except BadGlyphName:
-            # might happen when hallucinating
+            # might happen when hallucinating, what should we do?
             raise('BadGlyphName')
 
         self.can_corpse = bool(self.monster_glyph.corpse_spoiler)
@@ -123,30 +125,80 @@ class DMap():
     def update(self, player_location):
         self.visits_map[player_location] += 1
 
+class ThreatMap():
+    INVISIBLE_DAMAGE_THREAT = 6 # gotta do something lol
+    def __init__(self, glyph_grid, player_location_in_glyph_grid):
+        self.glyph_grid = glyph_grid
+        self.player_location_in_glyph_grid = player_location_in_glyph_grid
+
+        self.calculate_threat()
+
+    def calculate_threat(self):
+        melee_n_threat = np.zeros_like(self.glyph_grid)
+        melee_damage_threat = np.zeros_like(self.glyph_grid)
+
+        ranged_n_threat = np.zeros_like(self.glyph_grid)
+        ranged_damage_threat = np.zeros_like(self.glyph_grid)
+
+        it = np.nditer(self.glyph_grid, flags=['multi_index'])
+        for g in it:
+            glyph = gd.GLYPH_NUMERAL_LOOKUP[int(g)]
+            if it.multi_index != self.player_location_in_glyph_grid:
+                try:
+                    isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee
+                except AttributeError:
+                    if environment.env.debug: import pdb; pdb.set_trace()
+
+                if (isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee) or isinstance(glyph, gd.InvisibleGlyph or isinstance(glyph, gd.SwallowGlyph)):
+                    row_slice, col_slice = utilities.centered_slices_bounded_on_array(it.multi_index, (1, 1), self.glyph_grid) # radius one box around the location of g
+                    melee_n_threat[row_slice, col_slice] += 1 # monsters threaten their own squares in this implementation OK? TK 
+
+                    if isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee:
+                        melee_damage_threat[row_slice, col_slice] += glyph.monster_spoiler.melee_attack_bundle.max_damage
+
+                    if isinstance(glyph, gd.InvisibleGlyph):
+                        melee_damage_threat[row_slice, col_slice] += self.__class__.INVISIBLE_DAMAGE_THREAT # how should we imagine the threat of invisible monsters?
+
+                    if isinstance(glyph,gd.SwallowGlyph):
+                        melee_damage_threat[row_slice, col_slice] = gd.GLYPH_NUMERAL_LOOKUP[glyph.swallowing_monster_offset].monster_spoiler.engulf_attack_bundle.max_damage # while we're swallowed, all threat should be homogeneous
+
+                if (isinstance(glyph, gd.MonsterGlyph) and glyph.has_ranged):
+                    can_hit_mask = self.raytrace_threat(it.multi_index)
+                    ranged_n_threat[can_hit_mask] += 1
+                    ranged_damage_threat[can_hit_mask] += glyph.monster_spoiler.ranged_attack_bundle.max_damage
+
+        self.melee_n_threat = melee_n_threat
+        self.melee_damage_threat = melee_damage_threat
+
+        self.ranged_n_threat = ranged_n_threat
+        self.ranged_damage_threat = ranged_damage_threat
+
+    def raytrace_threat(self, source):
+        row_lim = self.glyph_grid.shape[0]
+        col_lim = self.glyph_grid.shape[1]
+
+        ray_offsets = physics.action_deltas
+
+        masks = []
+        for offset in ray_offsets:
+            ray_mask = np.full_like(self.glyph_grid, False, dtype='bool')
+
+            current = source
+            current = (current[0]+2*offset[0], current[1]+2*offset[1]) # initial bump so that ranged attacks don't threaten adjacent squares
+            while 0 <= current[0] < row_lim and 0 <= current[1] < col_lim:
+                glyph = gd.GLYPH_NUMERAL_LOOKUP[self.glyph_grid[current]]
+                if isinstance(glyph, gd.CMapGlyph) and glyph.is_wall: # is this the full extent of what blocks projectiles/rays?
+                    break # should we do anything with bouncing rays
+                ray_mask[current] = True
+
+                current = (current[0]+offset[0], current[1]+offset[1])
+
+            masks.append(ray_mask)
+
+        can_hit_mask = np.logical_or.reduce(masks)
+        return can_hit_mask
+
 class Neighborhood():
-    action_grid = np.array([
-        nethack.actions.CompassDirection.NW,
-        nethack.actions.CompassDirection.N,
-        nethack.actions.CompassDirection.NE,
-        nethack.actions.CompassDirection.W,
-        nethack.actions.MiscDirection.WAIT, # maybe this should be None so we can catch unexpected behavior?
-        nethack.actions.CompassDirection.E,
-        nethack.actions.CompassDirection.SW,
-        nethack.actions.CompassDirection.S,
-        nethack.actions.CompassDirection.SE,
-    ]).reshape(3,3)
-
-    action_to_delta = {
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.NW]: (-1, -1),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.N]: (-1, 0),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.NE]: (-1, 1),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.W]: (0, -1),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.E]: (0, 1),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.SW]: (1, -1),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.S]: (1, 0),
-        utilities.ACTION_LOOKUP[nethack.actions.CompassDirection.SE]: (1, 1),
-    }
-
     row_offset_grid = np.array([
         [-1, -1, -1,],
         [0, 0, 0,],
@@ -159,100 +211,6 @@ class Neighborhood():
         [-1, 0, 1,],
     ])
 
-    diagonal_moves = np.vectorize(lambda dir: utilities.ACTION_LOOKUP[dir] > 3 and utilities.ACTION_LOOKUP[dir] < 8)(action_grid)
-
-    @staticmethod
-    def centered_slices_bounded_on_array(start, radii, target_array):
-        row_slice_radius, col_slice_radius = radii
-        col_lim = target_array.shape[1]
-        row_lim = target_array.shape[0]
-
-        row_start, col_start = start
-
-        row_slice = slice(max(row_start-row_slice_radius, 0), min(row_start+row_slice_radius+1, row_lim)) # +1 because non-inclusive on upper end
-        col_slice = slice(max(col_start-col_slice_radius, 0), min(col_start+col_slice_radius+1, col_lim))
-
-        return row_slice, col_slice
-
-    @staticmethod
-    def move_slice_center(old_center, new_center, slices):
-        old_center_row, old_center_col = old_center
-        new_center_row, new_center_col = new_center
-
-        row_translate = old_center_row - new_center_row
-        col_translate = old_center_col - new_center_col
-
-        row_slice, col_slice = slices
-
-        relative_row_slice = slice(row_slice.start-row_translate,row_slice.stop-row_translate)
-        relative_col_slice = slice(col_slice.start-col_translate,col_slice.stop-col_translate)
-
-        return relative_row_slice, relative_col_slice
-
-    @classmethod
-    def raytrace_threat(cls, glyph_grid, source):
-        row_lim = glyph_grid.shape[0]
-        col_lim = glyph_grid.shape[1]
-
-        ray_offsets = cls.action_to_delta.values()
-
-        masks = []
-        for offset in ray_offsets:
-            ray_mask = np.full_like(glyph_grid, False, dtype='bool')
-
-            current = source
-            current = (current[0]+2*offset[0], current[1]+2*offset[1]) # initial bump so that ranged attacks don't threaten adjacent squares
-            while 0 <= current[0] < row_lim and 0 <= current[1] < col_lim:
-                glyph = gd.GLYPH_NUMERAL_LOOKUP[glyph_grid[current]]
-                if isinstance(glyph, gd.CMapGlyph) and glyph.is_wall: # is this the full extent of what blocks projectiles/rays?
-                    break # should we do anything with bouncing rays
-
-                ray_mask[current] = True
-
-                current = (current[0]+offset[0], current[1]+offset[1])
-
-
-            masks.append(ray_mask)
-
-        can_hit_mask = np.logical_or.reduce(masks)
-        return can_hit_mask
-
-    def calculate_threat(self, glyph_grid, player_location_in_glyph_grid):
-        INVISIBLE_DAMAGE_THREAT = 6 # gotta do something lol
-
-        n_threat_map = np.zeros_like(glyph_grid)
-        damage_threat_map = np.zeros_like(glyph_grid)
-
-        it = np.nditer(glyph_grid, flags=['multi_index'])
-        for g in it:
-            glyph = gd.GLYPH_NUMERAL_LOOKUP[int(g)]
-            if it.multi_index != player_location_in_glyph_grid:
-                try:
-                    isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee
-                except AttributeError:
-                    if environment.env.debug: import pdb; pdb.set_trace()
-
-                if (isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee) or isinstance(glyph, gd.InvisibleGlyph or isinstance(glyph, gd.SwallowGlyph)):
-                    row_slice, col_slice = Neighborhood.centered_slices_bounded_on_array(it.multi_index, (1, 1), glyph_grid) # radius one box around the location of g
-                    n_threat_map[row_slice, col_slice] += 1 # monsters threaten their own squares in this implementation OK? TK 
-
-                    if isinstance(glyph, gd.MonsterGlyph) and glyph.has_melee:
-                        damage_threat_map[row_slice, col_slice] += glyph.monster_spoiler.melee_attack_bundle.max_damage
-
-                    if isinstance(glyph, gd.InvisibleGlyph):
-                        damage_threat_map[row_slice, col_slice] += INVISIBLE_DAMAGE_THREAT # how should we imagine the threat of invisible monsters?
-
-                    if isinstance(glyph,gd.SwallowGlyph):
-                        damage_threat_map[row_slice, col_slice] += gd.GLYPH_NUMERAL_LOOKUP[glyph.swallowing_monster_offset].monster_spoiler.engulf_attack_bundle.max_damage/8 # stomachs do approx 1/8 of the monster damage 
-
-                if (isinstance(glyph, gd.MonsterGlyph) and glyph.has_ranged):
-                    can_hit_mask = self.__class__.raytrace_threat(glyph_grid, it.multi_index)
-                    n_threat_map[can_hit_mask] += 1
-                    damage_threat_map[can_hit_mask] += glyph.monster_spoiler.ranged_attack_bundle.max_damage
-
-
-        return (n_threat_map, damage_threat_map)
-
     def __init__(self, player_location, observation, dmap, previous_glyph_on_player, latest_monster_death):
         blstats = BLStats(observation['blstats'])
         self.player_location = player_location
@@ -260,19 +218,19 @@ class Neighborhood():
 
         window_size = 1
 
-        row_slice, col_slice = Neighborhood.centered_slices_bounded_on_array(player_location, (window_size, window_size), observation['glyphs'])
+        row_slice, col_slice = utilities.centered_slices_bounded_on_array(player_location, (window_size, window_size), observation['glyphs'])
 
         # a window into the action grid of the size size and shape as our window into the glyph grid (ie: don't include actions out of bounds on the map)
-        action_grid_row_slice, action_grid_col_slice = Neighborhood.move_slice_center(player_location, (1,1), (row_slice,col_slice)) # move center to (1,1) (action grid center)
+        action_grid_row_slice, action_grid_col_slice = utilities.move_slice_center(player_location, (1,1), (row_slice,col_slice)) # move center to (1,1) (action grid center)
 
-        self.action_grid = self.__class__.action_grid[action_grid_row_slice, action_grid_col_slice]
-        diagonal_moves = self.__class__.diagonal_moves[action_grid_row_slice, action_grid_col_slice]
+        self.action_grid = physics.action_grid[action_grid_row_slice, action_grid_col_slice]
+        diagonal_moves = physics.diagonal_moves[action_grid_row_slice, action_grid_col_slice]
 
         self.raw_glyphs = observation['glyphs'][row_slice, col_slice]
         self.glyphs = utilities.vectorized_map(lambda g: gd.GLYPH_NUMERAL_LOOKUP.get(g), self.raw_glyphs)
 
         self.visits = dmap.visits_map[row_slice, col_slice]
-        self.players_square_mask = self.action_grid == self.__class__.action_grid[1,1] # if the direction is the direction towards our square, we're not interested
+        self.players_square_mask = self.action_grid == physics.action_grid[1,1] # if the direction is the direction towards our square, we're not interested
 
         x,y = np.where(self.players_square_mask)
         self.player_location_in_neighborhood = list(zip(x,y))[0]
@@ -285,15 +243,15 @@ class Neighborhood():
 
         self.previous_glyph_on_player = previous_glyph_on_player
 
-        large_row_window, large_col_window = Neighborhood.centered_slices_bounded_on_array(player_location, (window_size+1, window_size+1), observation['glyphs'])
+
+        large_row_window, large_col_window = utilities.centered_slices_bounded_on_array(self.player_location, (window_size+1, window_size+1), observation['glyphs'])
         player_location_in_glyph_grid = (self.player_row-large_row_window.start, self.player_col-large_col_window.start)
-        threat_row_slice, threat_col_slice = Neighborhood.move_slice_center(self.player_location, player_location_in_glyph_grid, (row_slice, col_slice))
+        threat_row_slice, threat_col_slice = utilities.move_slice_center(self.player_location, player_location_in_glyph_grid, (row_slice, col_slice))
 
-        raw_n_threat, raw_damage_threat = self.calculate_threat(observation['glyphs'][large_row_window,large_col_window], player_location_in_glyph_grid)
-        self.n_threat = raw_n_threat[threat_row_slice,threat_col_slice]
-        self.damage_threat = raw_damage_threat[threat_row_slice,threat_col_slice]
+        threat_map = ThreatMap(observation['glyphs'][large_row_window,large_col_window], player_location_in_glyph_grid)
+        self.n_threat = threat_map.melee_n_threat[threat_row_slice,threat_col_slice]
+        self.damage_threat = threat_map.melee_damage_threat[threat_row_slice,threat_col_slice]
         self.threatened = self.n_threat > 0
-
         
         self.has_fresh_corpse = np.full_like(self.action_grid, False, dtype='bool')
         if latest_monster_death and latest_monster_death.can_corpse and (blstats.get('time') - latest_monster_death.time < ACCEPTABLE_CORPSE_AGE):
@@ -308,7 +266,7 @@ class Neighborhood():
 
     def glyph_set_to_directions(self, glyph_set):
         matches = np.isin(self.raw_glyphs, glyph_set)
-        directions = self.action_grid[matches]
+        directions = physics.action_grid[matches]
 
         return directions
 
@@ -325,6 +283,7 @@ BackgroundMenuPlan = menuplan.MenuPlan("background",{
     "Would you wear it for me?": utilities.ACTION_LOOKUP[nethack.actions.Command.ESC],
     "zorkmids worth of damage!": utilities.ACTION_LOOKUP[nethack.actions.Command.ESC],
     "little trouble lifting": utilities.ACTION_LOOKUP[nethack.actions.Command.ESC],
+    "For what do you wish?": utilities.ACTION_LOOKUP[nethack.actions.Command.ESC], # :(
 })
 
 class Character(NamedTuple):
@@ -386,6 +345,7 @@ class RunState():
         self.active_menu_plan = BackgroundMenuPlan
         self.message_log = []
         self.action_log = []
+        self.advice_log = []
         self.actions_without_consequence = []
 
         self.last_non_menu_action = None
@@ -505,9 +465,10 @@ class RunState():
     def log_message(self, message):
         self.message_log.append(message)
 
-    def log_action(self, action, menu_plan=None):
+    def log_action(self, action, menu_plan=None, advice=None):
         self.menu_plan_log.append(menu_plan)
         self.action_log.append(action)
+        self.advice_log.append(advice)
 
         if menu_plan == None:
             self.last_non_menu_action = action
@@ -606,7 +567,10 @@ class CustomAgent(BatchedAgent):
             # TODO need to get better at knowing the square where the monster dies
             # currently bad at ranged attacks, confusion, and more
             if not run_state.last_non_menu_action == utilities.ACTION_LOOKUP[nethack.actions.Command.FIRE]:
-                delta = Neighborhood.action_to_delta[run_state.last_non_menu_action]
+                try:
+                    delta = physics.action_to_delta[run_state.last_non_menu_action]
+                except:
+                    import pdb; pdb.set_trace()
                 recorded_death = RecordedMonsterDeath(
                     (player_location[0] + delta[0], player_location[1] + delta[1]),
                     blstats.get('time'),
@@ -687,7 +651,8 @@ class CustomAgent(BatchedAgent):
             retval = utilities.ACTION_LOOKUP[nethack.actions.Command.SEARCH]
             menu_plan = None
             #if environment.env.debug: pdb.set_trace()
-        run_state.log_action(retval)
+
+        run_state.log_action(retval, advice=chosen_advice)
 
         if menu_plan is not None:
             run_state.set_menu_plan(menu_plan)
--- utilities.py
+++ utilities.py
@@ -27,5 +27,35 @@ def have_item_oclasses(oclasses, inventory):
         if gd.ObjectGlyph.OBJECT_CLASSES.index(oclass) in inventory['inv_oclasses']: return True
     return False
 
+##################################
+### For neighborhoods and maps ###
+##################################
+
 def vectorized_map(f, nd_array):
     return np.array(list(map(f, nd_array.ravel()))).reshape(nd_array.shape)
+
+def centered_slices_bounded_on_array(start, radii, target_array):
+    row_slice_radius, col_slice_radius = radii
+    col_lim = target_array.shape[1]
+    row_lim = target_array.shape[0]
+
+    row_start, col_start = start
+
+    row_slice = slice(max(row_start-row_slice_radius, 0), min(row_start+row_slice_radius+1, row_lim)) # +1 because non-inclusive on upper end
+    col_slice = slice(max(col_start-col_slice_radius, 0), min(col_start+col_slice_radius+1, col_lim))
+
+    return row_slice, col_slice
+
+def move_slice_center(old_center, new_center, slices):
+    old_center_row, old_center_col = old_center
+    new_center_row, new_center_col = new_center
+
+    row_translate = old_center_row - new_center_row
+    col_translate = old_center_col - new_center_col
+
+    row_slice, col_slice = slices
+
+    relative_row_slice = slice(row_slice.start-row_translate,row_slice.stop-row_translate)
+    relative_col_slice = slice(col_slice.start-col_translate,col_slice.stop-col_translate)
+
+    return relative_row_slice, relative_col_slice
