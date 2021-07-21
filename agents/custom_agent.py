b35eb4b4d762aa33af336f7c3de4d855a9c486b7
Thayer Anderson <thayer@Thayers-MacBook-Pro.local>
2021-07-21 14:17:12 -0400

    If you're on a doorway, don't move into the shopkeeper's rectangle.

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -321,6 +321,8 @@ class Neighborhood(): # goal: mediates all access to glyphs by advisors
         self.previous_glyph_on_player = previous_glyph_on_player
         self.absolute_player_location = absolute_player_location
 
+        on_doorway = isinstance(previous_glyph_on_player, gd.CMapGlyph) and previous_glyph_on_player.is_open_door or feedback.diagonal_out_of_doorway_message
+
         #############################
         ### FULL EXTENT OF VISION ###
         #############################
@@ -341,6 +343,7 @@ class Neighborhood(): # goal: mediates all access to glyphs by advisors
         # radius 1 box around player in vision glyphs
         neighborhood_rows, neighborhood_cols = utilities.centered_slices_bounded_on_array(player_location_in_extended, (1, 1), extended_visible_glyphs)
         neighborhood_view = (neighborhood_rows, neighborhood_cols)
+
 
         ##############################
         ### RESTRICTED ACTION GRID ###
@@ -368,20 +371,37 @@ class Neighborhood(): # goal: mediates all access to glyphs by advisors
         self.raw_glyphs = extended_visible_raw_glyphs[neighborhood_view]
         self.glyphs = extended_visible_glyphs[neighborhood_view]
         self.visits = extended_visits[neighborhood_view]
+        is_open_door = utilities.vectorized_map(lambda g: isinstance(g, gd.CMapGlyph) and g.is_open_door, self.glyphs)
         self.is_monster = utilities.vectorized_map(lambda g: isinstance(g, gd.MonsterGlyph) or isinstance(g, gd.SwallowGlyph) or isinstance(g, gd.InvisibleGlyph) or isinstance(g, gd.WarningGlyph), self.glyphs)
 
+        ####################
+        # SHOPKEEPER STUFF #
+        ####################
+        is_shopkeeper = utilities.vectorized_map(lambda g: isinstance(g, gd.MonsterGlyph) and g.is_shopkeeper, self.glyphs)
+        shopkeeper_present = is_shopkeeper.any()
+
+        shop = np.full_like(self.glyphs, False, dtype='bool')
+        if shopkeeper_present and on_doorway:
+            import pdb; pdb.set_trace()
+            it = np.nditer(is_shopkeeper, flags=['multi_index'])
+            for b in it:
+                if b: # if this is a shopkeeper
+                    # draw the rectangle containing the player and shopkeeper
+                    shop_row_slice, shop_col_slice = utilities.rectangle_defined_by_corners(self.local_player_location, it.multi_index)
+
+                    shop[shop_row_slice, shop_col_slice] = True
+
         walkable_tile = utilities.vectorized_map(lambda g: g.walkable(character), self.glyphs)
-        open_door = utilities.vectorized_map(lambda g: isinstance(g, gd.CMapGlyph) and g.is_open_door, self.glyphs)
-        on_doorway = isinstance(previous_glyph_on_player, gd.CMapGlyph) and previous_glyph_on_player.is_open_door or feedback.diagonal_out_of_doorway_message
 
         # in the narrow sense
-        self.walkable = walkable_tile & ~(diagonal_moves & open_door) & ~(diagonal_moves & on_doorway) # don't move diagonally into open doors
+        self.walkable = walkable_tile & ~(diagonal_moves & is_open_door) & ~(diagonal_moves & on_doorway) & ~shop # don't move diagonally into open doors
 
         #########################################
         ### MAPS DERVIED FROM EXTENDED VISION ###
         #########################################
         self.secret_door_map = SecretDoorMap(extended_visible_raw_glyphs, extended_visible_glyphs, player_location_in_extended)
         self.threat_map = ThreatMap(extended_visible_raw_glyphs, extended_visible_glyphs, player_location_in_extended)
+
 
         #########################################
         ### LOCAL PROPERTIES OF EXTENDED MAPS ###
