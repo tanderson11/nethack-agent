4b88823aaf93ddc7de12fd4161afe3f6210be59b
Thayer Anderson <thayer@Thayers-MacBook-Pro.local>
2021-07-21 09:25:30 -0400

    Fixed the match string for items that are for sale.

--- menuplan.py
+++ menuplan.py
@@ -214,8 +214,8 @@ class InteractiveInventoryMenu(InteractiveMenu):
         'healing potions': lambda x: (x.category == "Potions") & ("healing" in x.item_appearance),
         'extra weapons': lambda x: (x.category == "Weapons") & ("weapon in hand" not in x.item_equipped_status),
 
-        'comestibles': lambda x: x.category == "Comestibles" and "unpaid" not in x.item_equipped_status,
-        'armor': lambda x: x.category == "Armor" and "unpaid" not in x.item_equipped_status,
+        'comestibles': lambda x: x.category == "Comestibles" and "for sale" not in x.item_equipped_status,
+        'armor': lambda x: x.category == "Armor" and "for sale" not in x.item_equipped_status,
     }
 
     class MenuItem:
