223e38c293142c1931ca1e54e1c04c97341d6447
Christian Anderson <canderson13@gmail.com>
2021-09-19 21:53:49 -0700

    Typo fix

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -620,7 +620,7 @@ class CustomAgent(BatchedAgent):
 
         changed_square = False
         if run_state.current_square is None or run_state.current_square.dcoord != dcoord or run_state.current_square.location != player_location:
-            change_square = True
+            changed_square = True
 
             if run_state.character and run_state.character.held_by is not None:
                 run_state.character.held_by = None
