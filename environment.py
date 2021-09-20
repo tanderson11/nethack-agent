48fd953931e6fbbf7e902c9837df569a91db6a43
Christian Anderson <canderson13@gmail.com>
2021-09-19 21:32:41 -0700

    Speed up test runs with max score

--- environment.py
+++ environment.py
@@ -11,6 +11,7 @@ class EnvironmentVariable(NamedTuple):
     target_roles: set[str]
     wizard: bool
     use_seed_whitelist: bool
+    max_score: int
 
 def try_cast(type, var):
     if var is None:
@@ -24,14 +25,17 @@ def parse_target_roles(raw_str):
     return set([constants.BaseRole[s] for s in raw_str.split(',')])
 
 def make_environment(**kwargs):
-    environment = {'num_environments':try_cast(int, os.getenv("NLE_DEV_NUM_ENVIRONMENTS")),
-    'num_episodes':try_cast(int, os.getenv("NLE_DEV_NUM_EPISODES")),
-    'debug':(os.getenv("NLE_DEV_DEBUG") == "true"),
-    'print_seed':(os.getenv("NLE_DEV_PRINT_SEED") == "true"),
-    'log_runs':((os.getenv("NLE_DEV_LOG_RUNS") == "true")),
-    'target_roles':parse_target_roles(os.getenv("NLE_DEV_TARGET_ROLES")),
-    'wizard':(os.getenv("NLE_DEV_WIZARD") == "true"),
-    'use_seed_whitelist':(os.getenv("NLE_USE_SEED_WHITELIST") == "true"),}
+    environment = {
+        'num_environments':try_cast(int, os.getenv("NLE_DEV_NUM_ENVIRONMENTS")),
+        'num_episodes':try_cast(int, os.getenv("NLE_DEV_NUM_EPISODES")),
+        'debug':(os.getenv("NLE_DEV_DEBUG") == "true"),
+        'print_seed':(os.getenv("NLE_DEV_PRINT_SEED") == "true"),
+        'log_runs':((os.getenv("NLE_DEV_LOG_RUNS") == "true")),
+        'target_roles':parse_target_roles(os.getenv("NLE_DEV_TARGET_ROLES")),
+        'wizard':(os.getenv("NLE_DEV_WIZARD") == "true"),
+        'use_seed_whitelist':(os.getenv("NLE_USE_SEED_WHITELIST") == "true"),
+        'max_score':try_cast(int, os.getenv("NLE_DEV_MAX_SCORE")),
+    }
 
     environment.update(kwargs)
 
