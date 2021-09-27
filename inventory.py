64bb508d8b35da102a18c693760c46ce53c17ba2
Thayer <tanderson11@gmail.com>
2021-09-27 11:01:05 -0400

    Don't pick up ammo of unknown BUC.

--- inventory.py
+++ inventory.py
@@ -314,7 +314,7 @@ class Weapon(Item):
         if self == character.inventory.wielded_weapon:
             return True
 
-        if (self.BUC is not None or self.enhancement is not None) and self.BUC != constants.BUC.cursed and (self.identity.is_ranged or self.identity.is_ammunition):
+        if self.BUC != constants.BUC.unknown and self.BUC != constants.BUC.cursed and (self.identity.is_ranged or self.identity.is_ammunition):
             return True
 
         is_better = self.instance_desirability_to_wield(character) > character.inventory.wielded_weapon.instance_desirability_to_wield(character)
