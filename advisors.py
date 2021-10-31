f8385772c828208bbd0d8fddebd9bff79c029867
Thayer <tanderson11@gmail.com>
2021-10-31 00:36:19 -0400

    Pure Elbereth respect; no more ranged damage.

--- advisors.py
+++ advisors.py
@@ -151,15 +151,6 @@ class Oracle():
         return (self.run_state.time - self.run_state.last_damage_timestamp)
 
     @utilities.cached_property
-    def turns_since_ranged_damage(self):
-        if self.run_state.last_ranged_damage_timestamp is None: return 0
-        return (self.run_state.time - self.run_state.last_ranged_damage_timestamp)
-
-    @utilities.cached_property
-    def recently_ranged_damaged(self):
-        return self.run_state.last_ranged_damage_timestamp is not None and self.turns_since_ranged_damage < 10
-
-    @utilities.cached_property
     def recently_damaged(self):
         return self.run_state.last_damage_timestamp is not None and (self.run_state.time - self.run_state.last_damage_timestamp < 10)
 
@@ -2131,10 +2122,6 @@ class EngraveElberethAdvisor(Advisor):
             return None
 
         if oracle.blind:
-            return None
-
-        if oracle.recently_ranged_damaged:
-            #import pdb; pdb.set_trace()
             return None
 
         for monster in run_state.neighborhood.adjacent_monsters:
