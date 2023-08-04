e14379fd4efd0749c2941bea8b322cebcc7c3b7b
Thayer Anderson <tanderson11@gmail.com>
2023-08-04 09:15:58 -0400

    Less aggressive about checking hung state (double travels from the same square can take lots of steps)

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -594,7 +594,7 @@ class RunState():
                 self.time_hung += 1
         else:
             self.time_hung = 0
-        if self.time_hung > 100:
+        if self.time_hung > 200:
             if environment.env.debug: pdb.set_trace()
             pass
         self.time = new_time
